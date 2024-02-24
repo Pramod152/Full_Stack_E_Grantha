6f435775c10e07341bdb897daad8e5205e5d4937
rajesh pandey <rajeshpandry24@gmail.com>
2024-02-24 22:41:59 +0545

    resolved conflict on merging

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import AboutPage from './pages/AboutPage/About';
-import CoursesPage from './pages/CoursePage/Course';
-import ContactPage from './pages/ContactPage/Contact';
-import LoginPage from './components/Login_SignUp/Login_SignUp';
-import U_Dashboard from './pages/UserDashboardPage/U_Dashboard';
-import Registration from './pages/RegistrationPage/Registration';
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage/HomePage";
+// import AboutPage from "./pages/AboutPage/About";
+// import CoursesPage from "./pages/CoursePage/Course";
+// import ContactPage from "./pages/ContactPage/Contact";
+// import LoginPage from "./components/Login_SignUp/Login_SignUp";
+// import U_Dashboard from "./pages/UserDashboardPage/U_Dashboard";
+// import Registration from "./pages/RegistrationPage/Registration";
 
 // const App = () => {
 //   return (
@@ -77,12 +77,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/course" element={<CoursesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/login" element={<Registration />} />
-        <Route path="/udashboard" element={<U_Dashboard />} />
+        {/* Other routes */}
+        <Route path="/E-Grantha/admin/allUser" element={<AllUsers />} />
       </Routes>
     </Router>
   );
