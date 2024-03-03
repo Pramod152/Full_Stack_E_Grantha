4f9df2b36ce6810be33bcc28536cb7fb9613ee7b
Pramod Ghimire <vc76it16@vedascollege.edu.np>
2024-03-03 21:23:18 +0545

    added coursetable.jsx file and created a sidenav and topbar

---
+++ Client/src/NewDesign/AdminPage/Component/SideNav.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SideNavContainer = styled.div`
+    background-color: red;
+    color: white;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+`;
+
+const NavItem = styled.div`
+    padding: 10px;
+    cursor: pointer;
+    font-size: 20px;
+`;
+const Div = styled.div`
+    margin-top: 30px;
+`;
+const Logo = styled.img`
+height: 50px;
+width: 50px;
+align-items: center;
+`;
+
+const SideNav = () => {
+    return (
+        <>
+        <SideNavContainer>
+            <Div>
+            <Logo as="img" src="/Logo_.png" alt="" />
+            <NavItem>Courses</NavItem>
+            <NavItem>Users</NavItem>
+            <NavItem>Dashboard</NavItem>
+            <NavItem>Profile</NavItem>
+            <NavItem>Logout</NavItem>
+            </Div>
+        </SideNavContainer>
+        </>
+    );
+}
+
+export default SideNav;
