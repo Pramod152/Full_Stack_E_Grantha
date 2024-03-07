76bfe87247f605bcbbc879d5ac5abe55dfb4fc2e
rajesh pandey <rajeshpandry24@gmail.com>
2024-03-07 22:06:26 +0545

    fuzzy search implemented to user controlller

--- Server/router/userRouter.js
+++ Server/router/userRouter.js
@@ -8,13 +8,13 @@ router.route("/signup").post(controller.signup);
 router.route("/login").post(controller.login);
 router.route("/secure").get(controller.secure);
 // router.route("/secure").get(auth, controller.secure);
-router.route("/getVideo/:videoId").get( controller.getVideo);
-router
-  .route("/getUserSubscriptions")
-  .get( controller.getUserSubscriptions);
-router.route("/allVideos").get( controller.allVideos);
-router.route("/subscribe/:videoId").post(auth , controller.subscribe);
+router.route("/getVideo/:videoId").get(controller.getVideo);
+router.route("/getUserSubscriptions").get(controller.getUserSubscriptions);
+router.route("/allVideos").get(controller.allVideos);
+router.route("/subscribe/:videoId").post(auth, controller.subscribe);
 router.route("/unsubscribe/:videoId").delete(controller.unsubscribe);
+
+router.route("/fuzzySearch").get(controller.fuzzySearch);
 
 ////contact us
 router.route("/contact").post(controller.contact);
